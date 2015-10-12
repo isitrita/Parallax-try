0a145155ebb77de353967e7231246c550cd4e7b6
is_it_rita <candor.cane@gmail.com>
2015-10-12 18:06:49 +0400

    added appearing linked with height of browser

--- js/functions.js
+++ js/functions.js
@@ -14,10 +14,10 @@ $('.fore-bird').css({
  'transform' : 'translate(0px, - '+ wScroll/40 +'%)'
 });
 
-if (wScroll > $('.clothes-pics').offset().top){
+if (wScroll > $('.clothes-pics').offset().top - ($(window).height()/1.2)){
 
       $('.clothes-pics figure').each(function(i){
-        
+
           setTimeout(function(){
           $('.clothes-pics figure').eq(i).addClass('is_showing');
         }, 150 * (i+1));
